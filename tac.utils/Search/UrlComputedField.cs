86f75760eb5cc12c9ac3c13ec1c014fced40a1fb
Tammi Anthony <tanthony2020@fau.edu>
2020-11-11 19:32:20 +0000

    initial checkin

---
+++ tac.utils/Search/UrlComputedField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data.Items;
+using Sitecore.Links;
+using Sitecore.Configuration;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+
+namespace TAC.Utils.Search
+{
+    public class UrlComputedField:AbstractComputedIndexField
+    {
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            Item item = indexable as SitecoreIndexableItem;
+            if (item == null)
+            {
+                return null;
+            }
+            UrlOptions defaultUrlOptions = LinkManager.GetDefaultUrlOptions();
+            defaultUrlOptions.SiteResolving = true;
+
+            var calculateUrl = LinkManager.GetItemUrl(item, defaultUrlOptions);
+            return calculateUrl;
+        }
+    }
+}
