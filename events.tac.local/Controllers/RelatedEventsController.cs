4a4eeac0b854b0f47570140478cac150efdc078b
Tammi Anthony <tanthony2020@fau.edu>
2020-11-12 21:08:58 +0000

    Module 9

---
+++ events.tac.local/Controllers/RelatedEventsController.cs
@@ -0,0 +1,29 @@
+﻿using events.tac.local.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class RelatedEventsController : Controller
+    {
+        // GET: RelatedEvents
+        public ActionResult Index()
+        {
+            var item = RenderingContext.Current.ContextItem;
+            MultilistField relatedEvents = item.Fields["RelatedEvents"];
+            var events = relatedEvents.GetItems()
+            .Select(i => new NavigationItem()
+            {
+                Title = i.DisplayName,
+                URL = LinkManager.GetItemUrl(i)
+            });
+            return View(events);
+        }
+    }
+}
