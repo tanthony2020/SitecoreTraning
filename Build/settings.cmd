6e468c3009477aa2adf3fc73fec26b73c55e4cf2
Tammi Anthony <tanthony2020@fau.edu>
2020-11-13 22:24:28 +0000

    serialization

---
+++ Build/settings.cmd
@@ -0,0 +1,7 @@
+@echo off
+REM locations for programs
+set msbuild="%programfiles(x86)%\MSbuild\14.0\bin\msbuild.exe"
+set wwwroot=c:\inetpub\wwwroot
+set packageoutput="%0\..\output"
+set serializationfolder=s.items
+set tools="%0\..\Tools"
